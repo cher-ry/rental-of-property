36f8c66858ab99848a2a16c20ef8da5f51ee570c
Chodr <choduraatyulyush@mail.ru>
2022-10-18 16:49:02 +0300

    init done

---
+++ app.js
@@ -0,0 +1,31 @@
+require('@babel/register');
+
+const express = require('express');
+// const session = require('express-session');
+
+const config = require('./config/serverConfig');
+const { sequelize } = require('./db/models');
+
+const app = express();
+
+const PORT = process.env.PORT ?? 3000;
+
+// routers
+
+// здесь подключаем роуты
+// const mainRouter = require('./routes/main.route');
+
+config(app);
+
+// здесь запускаем роуты
+// app.use('/', mainRouter);
+
+sequelize.authenticate({ logging: false });
+
+app.listen(PORT, async () => {
+  try {
+    console.log(`Server started ${PORT}`);
+  } catch (error) {
+    console.log("something's wrong");
+  }
+});
---
+++ config/serverConfig.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const morgan = require('morgan');
+
+const session = require('express-session');
+
+const sessionConfig = require('./sessionConfig');
+
+const { resLocals, getUser } = require('../middleware/login');
+
+const ssr = require('../middleware/ssr');
+
+const config = (app) => {
+  app.use(morgan('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'));
+  app.use(session(sessionConfig));
+  app.use(resLocals);
+  app.use(getUser);
+  app.use(ssr);
+};
+
+module.exports = config;
