0360f4830b0517179c230b89f4d16e627e44635c
Sophia <sonyalit@yandex.ru>
2022-10-19 15:38:30 +0300

    post router and put router in progress

--- routes/render/adminRouter.js
+++ routes/render/adminRouter.js
@@ -15,12 +15,32 @@ router.route('/new').get(async (req, res) => {
     res.renderComponent(NewAdd);
   });
 
-router.route('/:id/edit').get(async (req, res) => {
+router.route('/:id/edit')
+.get(async (req, res) => {
     const {id} = req.params
     const add  = await Article.findOne({where:{
         id
     }})
     res.renderComponent(EditAdd,{add});
-  });
+  })
+.put(async(req,res)=>{
+    try {
+        const entry = await Entry.update({
+          title: req.body.title,
+          body: req.body.body,
+        }, {
+          where: { id: req.params.id },
+          returning: true,
+          plain: true,
+        });
+    
+        res.json({ isUpdateSuccessful: true, entryID: entry[1].id });
+      } catch (error) {
+        res.json({
+          isUpdateSuccessful: false,
+          errorMessage: 'Не удалось обновить запись в базе данных.',
+        });
+      }
+})
 
 module.exports = router;
